31510c356edfadd1c496204f3a0fec948f0558c6
otreblan <otreblain@gmail.com>
2022-05-04 07:38:06 -0500

    Apply haar

--- proyecto_1/image.py
+++ proyecto_1/image.py
@@ -3,11 +3,12 @@ import pywt
 
 from PIL import Image
 
-def process_image(path: str, size: int = 64) -> np.ndarray:
+def process_image(path: str, size: int = 64, n: int = 4) -> np.ndarray:
     with Image.open(path).convert("L").resize((size, size)) as img:
         LL = np.array(img.getdata(), dtype=np.float32)/255
         LL = LL.reshape(img.size[0], img.size[1])
 
-        # TODO apply haar
+        for _ in range(n):
+            LL = pywt.dwt2(LL, "haar")[0]
 
         return LL.reshape(-1)
