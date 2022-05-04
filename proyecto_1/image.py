54a8a7547e2491f61e922b505f92d59d04daf2ff
otreblan <otreblain@gmail.com>
2022-05-04 07:29:49 -0500

    Resize image

--- proyecto_1/image.py
+++ proyecto_1/image.py
@@ -3,11 +3,11 @@ import pywt
 
 from PIL import Image
 
-def process_image(path: str) -> np.ndarray:
-    with Image.open(path).convert("L") as img:
+def process_image(path: str, size: int = 64) -> np.ndarray:
+    with Image.open(path).convert("L").resize((size, size)) as img:
         LL = np.array(img.getdata(), dtype=np.float32)/255
         LL = LL.reshape(img.size[0], img.size[1])
 
-        # TODO resize image and apply haar
+        # TODO apply haar
 
         return LL.reshape(-1)
