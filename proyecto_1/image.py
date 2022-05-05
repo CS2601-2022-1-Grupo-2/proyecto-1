7af86e50f77a024ef54fa8c9d95c6cb42d61e8a8
otreblan <otreblain@gmail.com>
2022-05-05 10:17:31 -0500

    Get true_y

--- proyecto_1/image.py
+++ proyecto_1/image.py
@@ -1,5 +1,4 @@
 import numpy as np
-import os
 import pywt
 
 from PIL import Image
@@ -13,19 +12,3 @@ def process_image(path: str, size: int = 64, n: int = 4) -> np.ndarray:
             LL = pywt.dwt2(LL, "haar")[0]
 
         return LL.reshape(-1)
-
-def get_vectors(path: str, test: bool):
-    v = []
-    y = []
-
-    if test:
-        for characteristic in os.scandir(path):
-            for file in os.scandir(characteristic):
-                v.append(process_image(file.path))
-                y.append(characteristic.name)
-    else:
-        for file in os.scandir(path):
-            v.append(process_image(file.path))
-            y.append(None)
-
-    return (np.array(v), y)
--- proyecto_1/svmknn.py
+++ proyecto_1/svmknn.py
@@ -1,26 +1,44 @@
 import os
 
-from .image import get_vectors
+import numpy as np
+import pandas as pd
+
+from .image import process_image
 from sklearn.neighbors import NearestNeighbors
 from collections import Counter
 
 class SvmKnn(object):
 
     def __init__(self, directory: str, method: str, k: int):
-        self.method = method
-        self.k      = k
+        self.directory = directory
+        self.method    = method
+        self.k         = k
 
         for file in os.scandir(directory):
-            if file.name == "Train":
+            if file.name == "Train.csv":
                 self.train_path: str = file.path
-            elif file.name == "Test":
+            elif file.name == "Test.csv":
                 self.test_path: str = file.path
+
+
+    def get_vectors(self, csv_path: str):
+        v = []
+        y = []
+
+        df = pd.read_csv(csv_path)
+
+        if isinstance(df, pd.DataFrame):
+            for i in range(len(df)):
+                v.append(process_image(os.path.join(self.directory, df.at[i, "Path"])))
+                y.append(df.at[i, "ClassId"])
+
+        return (np.array(v), y)
 
     def knnQuery(self, indices):
         return [Counter([self.y[i] for i in ii]).most_common(1)[0][0] for ii in indices]
 
     def train(self):
-        X, self.y = get_vectors(self.train_path, True)
+        X, self.y = self.get_vectors(self.train_path)
 
         if self.method == "knn":
             self.nbrs = NearestNeighbors(n_neighbors=self.k).fit(X)
@@ -28,7 +46,7 @@ class SvmKnn(object):
             print("TODO")
 
     def test(self):
-        X, _ = get_vectors(self.test_path, False)
+        X, true_y = self.get_vectors(self.test_path)
 
         if self.method == "knn":
             _, indices = self.nbrs.kneighbors(X)
