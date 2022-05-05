eb96c2155ad27feb79d0168f0c7e52ec1d7cac91
otreblan <otreblain@gmail.com>
2022-05-04 19:40:29 -0500

    Add --method option

--- proyecto_1/main.py
+++ proyecto_1/main.py
@@ -1,6 +1,6 @@
 import argparse
 
-from .svm import SvmAndKnn
+from .svmknn import SvmKnn
 
 def main():
     parser = argparse.ArgumentParser(description="Transit sign detector")
@@ -12,8 +12,17 @@ def main():
         help    = "Path of the directory with the train and test data"
     )
 
+    parser.add_argument(
+        "-m",
+        "--method",
+        type    = str,
+        help    = "svm or knn",
+        choices = ["svm", "knn"],
+        default = "knn"
+    )
+
     args = parser.parse_args()
 
-    svm = SvmAndKnn(args.directory)
+    svm = SvmKnn(args.directory, args.method)
     svm.train()
     svm.test()
--- proyecto_1/svmknn.py
+++ proyecto_1/svmknn.py
@@ -3,9 +3,11 @@ import os
 from .image import get_vectors
 from sklearn.neighbors import NearestNeighbors
 
-class SvmAndKnn(object):
+class SvmKnn(object):
 
-    def __init__(self, directory: str):
+    def __init__(self, directory: str, method: str):
+        self.method = method
+
         for file in os.scandir(directory):
             if file.name == "Train":
                 self.train_path: str = file.path
@@ -14,9 +16,17 @@ class SvmAndKnn(object):
 
     def train(self):
         X, self.y = get_vectors(self.train_path, True)
-        self.nbrs = NearestNeighbors(n_neighbors=2).fit(X)
+
+        if self.method == "knn":
+            self.nbrs = NearestNeighbors(n_neighbors=2).fit(X)
+        elif self.method == "svm":
+            print("TODO")
 
     def test(self):
         X, _ = get_vectors(self.test_path, False)
-        _, indices = self.nbrs.kneighbors(X)
-        print([[self.y[i] for i in ii] for ii in indices])
+
+        if self.method == "knn":
+            _, indices = self.nbrs.kneighbors(X)
+            print([[self.y[i] for i in ii] for ii in indices])
+        elif self.method == "svm":
+            print("TODO")
